a797291305876783b9b950f780ee54ab6afda53c
ilix <me@ilix.se>
2016-03-10 13:10:51 +0100

    [Infrastructure] Renamed env API_URL to API_HOST.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sed -i -- ""s/http/$API_PROTOCOL/g"" /usr/share/nginx/html/js/services/api.js
-sed -i -- ""s/localhost\:1337/$API_URL/g"" /usr/share/nginx/html/js/services/api.js
+sed -i -- ""s/localhost\:1337/$API_HOST/g"" /usr/share/nginx/html/js/services/api.js
 
 nginx -g "daemon off;"
