8d6839ed251971617d1d8be7f77fe5aed45e251d
Alexander Czigler <me@ilix.se>
2016-03-18 16:26:54 +0100

    [Refactoring] Moved workAreas controller to its own file.

---
+++ www/js/controllers/workAreas.js
@@ -0,0 +1,37 @@
+angular.module('app').controller('workAreasCtrl', function($scope, ProficiencyService, UserService, $state, $stateParams) {
+  $scope.user = []
+  $scope.profs = []
+
+  var userId = $stateParams.userId
+  UserService.get({ userId: userId }).then(function (data) {
+    $scope.user = data
+    if (data._source.professions) {
+      $scope.profs = data._source.professions
+    } else {
+      ProficiencyService.query({ id: 0 }).$promise.then(function (professions) {
+        professions.map(function (d) {
+          $scope.profs.push({
+            id: d._source.id,
+            name: d._source.namn,
+            selected: false
+          })
+        })
+      })
+    }
+  })
+
+  $scope.goToWork = function () {
+    return UserService
+      .update({
+        userId: $stateParams.userId,
+        professions: $scope.profs
+      })
+      .then(function () {
+        return $state.go('user.thankYou', { userId: $stateParams.userId })
+      })
+      .catch(function (error) {
+        console.log('error', error)
+        alert('Something went wrong!')
+      })
+  }
+})
