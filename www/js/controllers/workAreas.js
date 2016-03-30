49e06b18a192932f1b0c9fe919689b61d3b5ecd4
Alexander Czigler <me@ilix.se>
2016-03-30 20:56:56 +0200

    [WorkAreas] Now we only save selected profs.

--- www/js/controllers/workAreas.js
+++ www/js/controllers/workAreas.js
@@ -5,39 +5,52 @@ angular.module('app').controller('workAreasCtrl', function($scope, ProficiencySe
   var userId = $stateParams.userId
   UserService.get({ userId: userId }).then(function (data) {
     $scope.user = data
-    if (data._source.professions) {
-      $scope.profs = data._source.professions
-    } else {
-      ProficiencyService.query({ id: 0 }).$promise.then(function (professions) {
-        professions.map(function (d) {
-          $scope.profs.push({
-            id: d._source.id,
-            name: d._source.namn,
-            selected: false,
-            children: [{
-              id: 1000 + d._source.id,
-              name: 'subcat' + 1000 + d._source.id,
-              selected: false
-            },{
-              id: 1000 + d._source.id,
-              name: 'subcat' + 1000 + d._source.id,
-              selected: false
-            },{
-              id: 1000 + d._source.id,
-              name: 'subcat' + 1000 + d._source.id,
-              selected: false
-            }]
-          })
+    ProficiencyService.query({ id: 0 }).$promise.then(function (professions) {
+      professions.map(function (d) {
+        $scope.profs.push({
+          id: d._source.id,
+          name: d._source.namn,
+          selected: false,
+          children: [{
+            id: 1000 + d._source.id,
+            name: 'subcat' + 1000 + d._source.id,
+            selected: false
+          },
+          {
+            id: 1000 + d._source.id,
+            name: 'subcat' + 1000 + d._source.id,
+            selected: false
+          },
+          {
+            id: 1000 + d._source.id,
+            name: 'subcat' + 1000 + d._source.id,
+            selected: false
+          }]
         })
       })
-    }
+      ensureSelected(data._source.professions)
+    })
   })
+
+  function ensureSelected (professions) {
+    professions.filter(filterSelected).map(function (selectedProfession) {
+      $scope.profs.map(function (item) {
+        if (item.id === selectedProfession.id) {
+          item.selected = true
+        }
+      })
+    })
+  }
+
+  function filterSelected (item) {
+    return item.selected
+  }
 
   $scope.goToWork = function () {
     return UserService
       .update({
         userId: $stateParams.userId,
-        professions: $scope.profs
+        professions: $scope.profs.filter(filterSelected)
       })
       .then(function () {
         return $state.go('user.thankYou', { userId: $stateParams.userId })
