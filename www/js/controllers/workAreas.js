9e92e00a848fb1731332b048c3a0edc850f77401
Alexander Czigler <me@ilix.se>
2016-05-24 22:29:44 +0200

    Rearranged the code a little. Started preparing for reading the user's proficiencies when loading the view.

--- www/js/controllers/workAreas.js
+++ www/js/controllers/workAreas.js
@@ -1,55 +1,10 @@
 angular.module('app').controller('workAreasCtrl', function ($scope, ProficiencyService, UserService, $state, $stateParams) {
-  $scope.user = []
-  $scope.profs = []
-  $scope.children = []
-  $scope.loadedChildren = []
-
-  $scope.proficiencies = {}
-  $scope.userProficiencies = {}
-
-  // Tracks visible trees.
-  $scope.show = []
+  $scope.user = [] // The current user. Will be loaded on view init.
+  $scope.proficiencies = {} // An object holding loaded proficiencies.
+  $scope.userProficiencies = {} // An object holding true/false for user's proficiencies.
+  $scope.show = [] // Tracks visible sub trees.
 
   var userId = $stateParams.userId
-  UserService.get({ userId: userId }).then(function (data) {
-    $scope.user = data
-    ProficiencyService.query({ id: 0 }).$promise.then(function (professions) {
-      if (professions) {
-        professions.map(function (d) {
-          $scope.profs.push({
-            id: d._source.id,
-            name: d._source.namn,
-            parentId: 0,
-            selected: false
-          })
-        })
-      }
-      ensureSelected(data._source.professions)
-    })
-  })
-
-  $scope.loadChildren = function (parent) {
-    if ($scope.loadedChildren.indexOf(parent.id) > -1) {
-      return
-    }
-    $scope.loadedChildren.push(parent.id)
-    if (!parent.selected) {
-      return
-    }
-    ProficiencyService.query({ id: parent.id }).$promise.then(function (professions) {
-      if (professions) {
-        professions.map(function (d) {
-          $scope.children.push({
-            id: d._source.id,
-            name: d._source.namn,
-            parentId: parent.id,
-            selected: false
-          })
-        })
-      }
-      ensureSelected(professions)
-    })
-  }
 
   $scope.loadProficiencies = function (parentId, parentName) {
     var index = $scope.show.indexOf(parentId)
@@ -64,7 +19,7 @@ angular.module('app').controller('workAreasCtrl', function ($scope, ProficiencyS
           proficiencies.map(function (proficiency) {
             if (parentName) {
               $scope.proficiencies[parentName][proficiency._source.namn] = proficiency._source
-              $scope.userProficiencies[parentName][proficiency._source.namn] = false
+              $scope.userProficiencies[parentName][proficiency._source.namn] = true
             } else {
               $scope.proficiencies[proficiency._source.namn] = proficiency._source
               $scope.userProficiencies[proficiency._source.namn] = false
@@ -74,31 +29,11 @@ angular.module('app').controller('workAreasCtrl', function ($scope, ProficiencyS
       })
   }
 
-  function ensureSelected (professions) {
-    if (!professions) {
-      return
-    }
-    professions.filter(filterSelected).map(function (selectedProfession) {
-      $scope.profs.map(function (item) {
-        if (item.id === selectedProfession.id) {
-          item.selected = true
-          if (item.parentId === 0) {
-            $scope.loadChildren(item)
-          }
-        }
-      })
-    })
-  }
-
-  function filterSelected (item) {
-    return item.selected
-  }
-
   $scope.goToWork = function () {
     return UserService
       .update({
         userId: $stateParams.userId,
-        professions: $scope.profs.filter(filterSelected)
+        proficiencies: $scope.userProficiencies
       })
       .then(function () {
         return $state.go('user.thankYou', { userId: $stateParams.userId })
@@ -109,6 +44,15 @@ angular.module('app').controller('workAreasCtrl', function ($scope, ProficiencyS
       })
   }
 
-  // Do when view is loaded.
-  $scope.loadProficiencies(0)
+  // Get the user object and figure out selected proficiencies.
+  UserService.get({ userId: userId }).then(function (data) {
+    $scope.user = data._source
+  })
+  .then(function () {
+    Object.keys($scope.user.proficiencies).map(function (key) {
+      console.log('Processing key', key)
+    })
+    // Load all proficiencies.
+    $scope.loadProficiencies(0)
+  })
 })
