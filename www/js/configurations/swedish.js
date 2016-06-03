1f2432e3feed5775e7a9164cc4699ada147b6e92
believer <rickard.laurin@gmail.com>
2016-06-03 10:30:08 +0200

    Updated Swedish copy

--- www/js/configurations/swedish.js
+++ www/js/configurations/swedish.js
@@ -2,20 +2,20 @@ angular.module('app').config(function ($translateProvider) {
   $translateProvider.translations('sv', {
     "direction": "ltr",
 
-    "hero_title": "Låt jobben hitta dig istället!",
-    "hero_body": "Som nyanländ tar det tid innan du kan börja jobba. Genom att dela din nuvarande arbetsstatus och dina kompetenser även innan du har ett personnummer kan potentiella arbetsgivare få koll på att du finns och erbjuda praktikplatser.",
+    "hero_title": "Berätta om din kompetens",
+    "hero_body": "Som nyanländ och utan personnummer tar det tid innan man kan börja jobba. Dela din nuvarande arbetsstatus och dina kompetenser så potentiella arbetsgivare får koll på att du finns och kan erbjuda praktikplatser.",
     "hero_button": "Berätta om dig själv",
 
     "your_number": "Mobilnummer (ex. 0731234567)",
 
-    "about_title": "Det tar inte mer än en minut.",
-    "about_body" : "Genom att synliggöra din kompetens kan fler passande arbetstillfällen skapas. Att registrera sig är frivilligt och du behöver inte uppge ditt namn. Däremot behöver vi veta att du inte lämnar uppgifterna fler gånger så du måste först ange ditt mobilnummer.",
+    "about_title": "Det tar en minut.",
+    "about_body" : "Berätta om din kompetens och du är ett steg närmare ett arbetstillfälle.",
 
     "phone_label": "Fyll i ditt telefonnummer",
     "continue": "Gå vidare",
 
     "phone_error_missing": "Telefonnummer saknas",
-    "phone_error_malformed": "Telefonnummer är felformaterat",
+    "phone_error_malformed": "Telefonnummer är felformulerat",
 
     "age_label": "Ålder",
     "birthdate_label": "Födelsedatum",
@@ -53,8 +53,8 @@ angular.module('app').config(function ($translateProvider) {
     "skills_label": "Välj de yrkesområden som du har erfarenhet ifrån",
 
     "thanks_title": "Tack! Du har nu registrerat din profil",
-    "thanks_body": "Fantastiskt! Ju fler som registrerar sig - destå mer ökar chansen att fler får jobb. Hjälp oss genom att skicka detta via Viber/WhatsApp/Facebook/SMS.",
-    "share_body": "Jag har registrerat mina uppgifter på http://competency.se för att snabbare få jobb i Sverige. Vi behöver 1000 personer för att kunna börja göra utskick. Registrera dig också!",
+    "thanks_body": "Fantastiskt! Ju fler som registrerar sig - desto mer ökar chansen att fler får jobb. Hjälp oss genom att skicka detta via Viber/WhatsApp/Facebook/SMS.",
+    "share_body": "Jag har registrerat mina uppgifter på http://competency.se för att snabbare få jobb i Sverige. Vi behöver 1000 personer för att kunna börja skillnad. Registrera dig också!",
 
     "read_more": "Läs mer på Competency.se"
   });
