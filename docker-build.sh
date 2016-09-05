259354c069693ba6acc4fa2063f424f5d1eee66a
ilix <me@ilix.se>
2016-09-05 11:58:22 +0200

    Removed old build script.

--- docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# make sure you do docker login first...
-
-docker build -t competency-app .
-docker tag -f competency-app iteamoperations/competency-app
-docker push iteamoperations/competency-app
