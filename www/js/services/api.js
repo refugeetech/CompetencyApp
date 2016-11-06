b097927329013a185aa17f7b507573c84ba36c73
ilix <src@ilix.se>
2016-11-06 10:10:25 +0100

    Ensured baseUrl is automatically configured now that we use both .nu and .se

--- www/js/services/api.js
+++ www/js/services/api.js
@@ -1,5 +1,21 @@
 angular.module('app').service('API', function () {
+  var baseUrl = 'http://'
+  var port = window.location.port
+  var hostname = window.location.hostname
+
+  if (port === 443) {
+    baseUrl = 'https://'
+  }
+
+  if (hostname.indexOf('competency') > -1) {
+    baseUrl += hostname.replace('app.', '')
+  } else {
+    baseUrl += 'localhost:1337'
+  }
+
+  console.log('Automatically configured baseUrl', baseUrl)
+
   return {
-    BaseUrl: 'http://localhost:1337'
+    BaseUrl: baseUrl
   }
 })
